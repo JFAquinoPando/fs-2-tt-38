56c0c3f18c1a11e04e6e49f6a175ac6b7d99245d
JFAquinoPando <pandolcik@gmail.com>
2026-02-19 23:29:23 -0300

    ew

--- config/db.js
+++ config/db.js
@@ -3,8 +3,6 @@ import "dotenv/config";
 
 mongoose.set("bufferCommands", false);
 
-const { MONGO_URI } = process.env;
-
 const estadoConexionGlobal = globalThis.__mongooseEstado ?? {
     conexion: null,
     promesa: null,
@@ -13,26 +11,33 @@ const estadoConexionGlobal = globalThis.__mongooseEstado ?? {
 globalThis.__mongooseEstado = estadoConexionGlobal;
 
 export const conectarDB = async () => {
-    if (estadoConexionGlobal.conexion) {
+    if (
+        estadoConexionGlobal.conexion &&
+        mongoose.connection.readyState === 1
+    ) {
         return estadoConexionGlobal.conexion;
     }
+
+    const MONGO_URI = "mongodb+srv://jfaquinopando_db_user:yqjFu7zxPInR59jH@clausterfullstack.t6i9gun.mongodb.net/test?retryWrites=true&w=majority";
 
     if (!MONGO_URI) {
         throw new Error("La variable de entorno MONGO_URI no esta definida.");
     }
 
-    if (!estadoConexionGlobal.promesa) {
-        estadoConexionGlobal.promesa = mongoose
-            .connect(MONGO_URI, {
-                serverSelectionTimeoutMS: 15000,
-                socketTimeoutMS: 45000,
-                connectTimeoutMS: 10000,
-            })
-            .catch((error) => {
-                estadoConexionGlobal.promesa = null;
-                throw error;
-            });
-    }
+    estadoConexionGlobal.promesa = null;
+
+    estadoConexionGlobal.promesa = mongoose
+        .connect(MONGO_URI, {
+            dbName: "test",
+            serverSelectionTimeoutMS: 15000,
+            socketTimeoutMS: 45000,
+            connectTimeoutMS: 10000,
+        })
+        .catch((error) => {
+            estadoConexionGlobal.promesa = null;
+            estadoConexionGlobal.conexion = null;
+            throw error;
+        });
 
     estadoConexionGlobal.conexion = await estadoConexionGlobal.promesa;
     return estadoConexionGlobal.conexion;
