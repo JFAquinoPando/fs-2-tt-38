67367ae23c9003779f271af23f77c0814de8fc24
JFAquinoPando <pandolcik@gmail.com>
2026-02-19 21:55:57 -0300

    aumentando el tiempo de timeout

--- config/db.js
+++ config/db.js
@@ -5,12 +5,16 @@ export const conectarDB = async () => {
     try {
         const { MONGO_URI } = process.env
 
-       await connect(MONGO_URI)
+       await connect(MONGO_URI, {
+           serverSelectionTimeoutMS: 15000,
+           socketTimeoutMS: 45000,
+           connectTimeoutMS: 10000,
+       })
        console.log("conexión a MongoDB Atlas correcto 🫡");
        
        
     } catch (error) {
-        console.error("Lo lamento, tenemos un error 💔");
+        console.error("Lo lamento, tenemos un error 💔", error);
         process.exit(1)
     }
 }
